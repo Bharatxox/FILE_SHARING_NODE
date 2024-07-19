7d259f48d3e200addcb5ad86b994a5e0cf4b0933
Bharat Bisht <kamalbisht819@gmail.com>
2024-07-20 04:55:24 +0530

    atlas cloud config

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const fileRoutes = require("./router/file");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/file_app")
+  .connect(process.env.DATABASE_URL)
   .then(() => console.log("DB connection established"))
   .catch((err) => console.log("error connecting to file server", err));
 
 app.use(express.json());
 app.use(fileRoutes);
 
-const port = 8000;
+const port = 10000;
 
 app.listen(port, () => {
   console.log("server is runnind at the" + port);
